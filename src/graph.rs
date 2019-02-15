33752eb2f7df58523c5eee838323652c933455f0
Katharina Fey <kookie@spacekookie.de>
2019-02-15 10:56:16 +0100

    Work in progress, implementing various graph functions
    Two new functions added to the dependency graph:
    
    - `find_depending` which finds nodes in the graph which depend on given node
    - `find_optimal` which uses this information to find the node with the `max()`
      number of inside-graph dependents (packages depending on it)
    
    This code is far from done but adds a bunch of functionality.
    All that's left to do is Build a publish strategy where the nodes with
    the largest number of depending packages get published first.
    
    At this time it's not clear how to handle circular dependencies in this graph.
    If a circular dependency is detected, the program will currently run forever.
    
    There's a few different ways of going about it:
    
    1. Detect the circle and cancel to let the user deal with it manually.
       Later on, maybe the user could set an "ignore" file for those packages.
    2. Set a range dependency which allows for fuzzy circles to be published
       (See `diesel_cli`, `diesel_migration` and `barrel` circle)
    
    Are there other strategies not yet considered?

--- src/graph.rs
+++ src/graph.rs
@@ -18,6 +18,19 @@ pub struct Node {
     pub deps: Vec<Dep>,
 }
 
+impl Node {
+    /// Returns an `Option<&Dep>` if the node depends on it
+    fn depends_on(&self, name: &str) -> bool {
+        self.deps.iter().fold(false, |mut acc, dep| {
+            if dep.name == name {
+                acc = true;
+            }
+
+            acc
+        })
+    }
+}
+
 #[derive(Debug)]
 pub struct DepGraph {
     nodes: HashMap<String, Node>,
@@ -42,6 +55,75 @@ impl DepGraph {
 
     pub fn add_dependency(mut self, name: &str, dep: &Dep) -> Self {
         self.nodes.get_mut(name).unwrap().deps.push(dep.clone());
+        self
+    }
+
+    /// Goes through the graph and finds all packages that depend on `name`
+    pub fn find_depending(&self, name: &str) -> Vec<&Node> {
+        self.nodes.iter().fold(Vec::new(), |mut vec, (_, pkg)| {
+            if pkg.depends_on(name) {
+                vec.push(pkg);
+            }
+
+            vec
+        })
+    }
+
+    /// Find the ideal node to start publishing from
+    ///
+    /// This is a node which ideally has no dependencies in the graph
+    /// while many nodes depend on it.
+    pub fn find_optimal(&self) {
+        /// A simple local structure to tag nodes with
+        #[derive(Debug)]
+        struct NodeExt<'a> {
+            node: &'a Node,
+            rds: usize,
+        }
+
+        // Iterate through all nodes
+        let nodes = self
+            .nodes
+            .iter()
+            // Fold into lists of `NodeExt` tags
+            .fold(Vec::<NodeExt>::new(), |vec, (_, node)| {
+                vec.insert(
+                    // Iterate through all nodes again (This is O(N²) !)
+                    self.nodes
+                        .iter()
+                        // Fold search into `NodeExt` - increments rds if dependency found
+                        .fold(NodeExt { node, rds: 0 }, |ne, (_, onode)| NodeExt {
+                            rds: ne.rds + onode.depends_on(&node.name) as usize,
+                            ..ne
+                        }),
+                )
+            });
+
+        // for (_, node) in &self.nodes {
+        //     let mut ne = NodeExt { node, rds: 0 };
+
+        //     for onode in &self.nodes {
+        //         if onode.1.depends_on(&node.name) {
+        //             ne.rds += 1;
+        //         }
+        //     }
+
+        //     nodes.push(ne);
+        // }
+
+        println!("{:#?}", nodes);
+    }
+}
+
+/// A utility trait that makes inserting into collections more functional
+/// 🥁
+trait InsertSelf<T> {
+    fn insert(self, i: T) -> Self;
+}
+
+impl<T> InsertSelf<T> for Vec<T> {
+    fn insert(mut self, i: T) -> Self {
+        self.push(i);
         self
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-extern crate toml;
 extern crate toml_edit;
 
 use std::fs::File;
@@ -11,14 +10,14 @@ mod utils;
 use graph::DepGraph;
 
 fn main() {
-    if env::args().len() < 2 {
-        eprintln!("Usage: cargo ws-release <major|minor|patch|rc|beta|alpha>");
-        process::exit(255);
-    }
+    // if env::args().len() < 2 {
+    //     eprintln!("Usage: cargo ws-release <major|minor|patch|rc|beta|alpha>");
+    //     process::exit(255);
+    // }
 
-    let level = env::args().next().unwrap();
+    // let level = env::args().next().unwrap();
     match File::open("Cargo.toml") {
-        Ok(f) => do_batch_release(f, &level),
+        Ok(f) => do_batch_release(f, "yolo"),
         _ => {
             eprintln!("No `Cargo.toml` found to work with!");
             process::exit(2);
@@ -40,5 +39,7 @@ fn do_batch_release(f: File, lvl: &str) {
                 .fold(graph, |graph, dep| graph.add_dependency(&name, dep))
         });
 
-    println!("{:#?}", v);
+    // println!("{:#?}", v.find_depending("lockchain-core"));
+
+    v.find_optimal();
 }
